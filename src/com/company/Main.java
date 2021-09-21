02bc244c879e1890fa8934462c35abe58f08dcc7
faithhunt <huntfe20@wfu.edu>
2021-09-20 20:56:16 -0400

    pasted from canvas

---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	import java.util.Random;
+
+        public class Main {
+            public static void main(String[] args) {
+                Random rnd = new Random(14);
+                int[] ar = new int[20];
+                for (int i = 0; i < ar.length; i++) {
+                    ar[i] = rnd.nextInt(100) + 1;
+                }
+                print(ar);
+                System.out.println();
+                System.out.println("Bubblesort");
+                bubbleSort(ar);
+                print(ar);
+       /*for (int i = 0; i < ar.length; i++) {
+           ar[i] = rnd.nextInt(100) + 1;
+       }
+       System.out.println();
+       System.out.println("Insertion sort");
+       print(ar);
+       insertionSort(ar);
+       print(ar);*/
+
+            }
+
+            public static void insertionSort(int[] arr) {
+            }
+
+            public static void bubbleSort(int[] arr) {
+                for (int i = 0; i < arr.length - 1; i++) {
+
+                    for (int j = 0; j < arr.length - 1 - i; j++) {
+                        int indexOfLargest = 0;
+                        if (arr[j] > arr[j + 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                        if (arr[j] > arr[j + 1]) {
+                            indexOfLargest = j;
+
+                        }
+                    }
+           /*swap
+           int temp = arr[indexOfLargest];
+           arr[indexOfLargest] = arr[arr.length - 1 - i];*/
+                }
+            }
+
+            public static void print ( int[] arr){
+                for (int i = 0; i < arr.length - 1; i++) {
+                    System.out.print(arr[i] + " ");
+                }
+            }
+        }
+
+    }
+}
